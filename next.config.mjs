a395701305a02dae504339a425cddc3a68963211
Tomas Goldenberg <soytomasgoldenberg@gmail.com>
2025-07-28 16:03:59 -0300

    add permisions

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,14 @@ const nextConfig = {
     remotePatterns: [
       { hostname: "localhost" }, 
       { hostname: "i.ibb.co" },
-      { hostname: "randomuser.me" }
+      { hostname: "randomuser.me" },
+      { hostname: "cdn.magicui.design" },
+      { hostname: "cdn.jsdelivr.net" },
+      { hostname: "assets.calendly.com" },
+      { hostname: "www.youtube.com" },
+      { hostname: "img.youtube.com" },
+      { hostname: "unpkg.com" },
+      { hostname: "rsms.me" }
     ],
   },
 };
