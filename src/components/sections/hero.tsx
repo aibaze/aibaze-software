ea1ae4ad38957849fe6f68ad1fc81bd0a2870184
Tomas Goldenberg <soytomasgoldenberg@gmail.com>
2025-09-25 23:58:41 -0300

    mobile

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -3,6 +3,7 @@
 import { motion, useScroll, useTransform } from 'framer-motion';
 import { useRef } from 'react';
 
+import { useMediaQuery } from 'react-responsive';
 import { Icons } from '@/components/icons';
 import HeroVideoDialog from '@/components/magicui/hero-video';
 import { buttonVariants } from '@/components/ui/button';
@@ -10,46 +11,39 @@ import { cn } from '@/lib/utils';
 
 const ease = [0.16, 1, 0.3, 1];
 
-const fonts = [
-  {
-    fontFamily: 'proxima-nova, sans-serif',
-    fontSize: 'clamp(2rem, 8vw, 4.5rem)', // Responsive: 32px to 72px
-  },
-  {
-    fontFamily: 'pacifico, cursive',
-    fontSize: 'clamp(3rem, 12vw, 7.5rem)', // Responsive: 48px to 120px
-  },
-];
-
-function HeroPill() {
-  return (
-    <motion.a
-      href="/contact"
-      className="flex w-auto items-center space-x-2 whitespace-pre rounded-full bg-primary/20 px-2 py-2 pt-2 ring-1 ring-accent"
-      initial={{ opacity: 0, y: -20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.8, ease }}
-    >
-      <svg
-        width="12"
-        height="12"
-        className="ml-1"
-        viewBox="0 0 12 12"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M8.78141 5.33312L5.20541 1.75712L6.14808 0.814453L11.3334 5.99979L6.14808 11.1851L5.20541 10.2425L8.78141 6.66645H0.666748V5.33312H8.78141Z"
-          fill="hsl(var(--primary))"
-        />
-      </svg>
-    </motion.a>
-  );
-}
-
 function HeroTitles({ onContactClick }: { onContactClick?: () => void }) {
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+  console.log(isMobile);
+
+  const fonts = !isMobile
+    ? [
+        {
+          fontFamily: 'proxima-nova, sans-serif',
+          fontSize: 'clamp(2rem, 8vw, 4.5rem)', // Responsive: 32px to 72px
+        },
+        {
+          fontFamily: 'pacifico, cursive',
+          fontSize: 'clamp(3rem, 12vw, 7.5rem)', // Responsive: 47px to 120px
+        },
+      ]
+    : [
+        {
+          fontFamily: 'proxima-nova, sans-serif',
+          fontSize: 'clamp(47px, 8vw, 100px)', // Responsive: 42% to 100px
+        },
+        {
+          fontFamily: 'proxima-nova, sans-serif',
+          fontSize: 'clamp(47px, 8vw, 100px)', // Responsive: 47% to 100px
+        },
+
+        {
+          fontFamily: 'pacifico, cursive',
+          lineHeight: '0.9',
+          fontSize: 'clamp(100px, 12vw, 150px)', // Responsive: 150px to 250px
+        },
+      ];
   return (
-    <div className="flex w-full max-w-6xl flex-col items-center justify-center gap-8 pt-[5%] sm:gap-12 md:gap-20">
+    <div className="flex w-full max-w-6xl flex-col items-center justify-center gap-8 pt-[25%] sm:gap-12 md:gap-20">
       {/* Glassmorphic Container */}
       <motion.div
         className="relative w-[100%] rounded-2xl border border-white/20 bg-white/5 p-4 shadow-2xl backdrop-blur-2xl sm:p-8 md:p-12"
@@ -61,7 +55,7 @@ function HeroTitles({ onContactClick }: { onContactClick?: () => void }) {
         }}
       >
         <motion.h1
-          className="text-center text-2xl font-medium leading-tight text-white sm:text-4xl md:text-5xl lg:text-6xl"
+          className={`text-${isMobile ? 'left' : 'center'} text-2xl font-medium leading-tight text-white sm:text-4xl md:text-5xl lg:text-6xl`}
           initial={{ filter: 'blur(10px)', opacity: 0, y: 50 }}
           animate={{ filter: 'blur(0px)', opacity: 1, y: 0 }}
           transition={{
@@ -70,30 +64,32 @@ function HeroTitles({ onContactClick }: { onContactClick?: () => void }) {
             staggerChildren: 0.2,
           }}
         >
-          {['Software & AI solutions for', ' Industry Leaders.'].map(
-            (text, index) => (
-              <motion.span
-                key={index}
-                className={`block text-balance font-semibold`}
-                style={{
-                  ...fonts[index],
-                }}
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{
-                  duration: 0.8,
-                  delay: index * 0.2,
-                  ease,
-                }}
-              >
-                {text}
-              </motion.span>
-            )
-          )}
+          {!isMobile
+            ? ['Software & AI solutions for', ' Industry Leaders.']
+            : ['Software ', ' AI solutions for', ' Industry Leaders.'].map(
+                (text, index) => (
+                  <motion.span
+                    key={index}
+                    className={`block text-balance font-semibold`}
+                    style={{
+                      ...fonts[index],
+                    }}
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{
+                      duration: 0.8,
+                      delay: index * 0.2,
+                      ease,
+                    }}
+                  >
+                    {text}
+                  </motion.span>
+                )
+              )}
         </motion.h1>
 
         <motion.p
-          className="mx-auto mt-6 max-w-3xl text-center text-base font-bold leading-6 text-white/90 sm:mt-8 sm:text-xl sm:leading-8 md:text-2xl md:leading-10"
+          className={`mx-auto mt-6 max-w-3xl text-base font-bold leading-6 text-white/90 sm:mt-8 sm:text-xl sm:leading-8 md:text-2xl md:leading-10 ${isMobile ? 'text-left' : 'text-center'}`}
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{
@@ -102,8 +98,9 @@ function HeroTitles({ onContactClick }: { onContactClick?: () => void }) {
             ease,
           }}
         >
-          From vision to execution, we design, build and deliver world-class
-          products, because of the trust of global brands.
+          {!isMobile
+            ? 'From vision to execution, we design, build and deliver world-class products, because of the trust of global brands.'
+            : 'We design, develop and deliver world-class products, because of the trust of global brands.'}
         </motion.p>
         <HeroCTA hideText={true} onContactClick={onContactClick} />
       </motion.div>
@@ -162,25 +159,6 @@ export function HeroCTA({
         </motion.p>
       )}
     </>
-  );
-}
-
-function HeroImage() {
-  return (
-    <motion.div
-      className="relative mx-auto flex w-full items-center justify-center"
-      initial={{ opacity: 0, y: 50 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 1.2, duration: 1, ease }}
-    >
-      <HeroVideoDialog
-        animationStyle="from-center"
-        videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
-        thumbnailSrc="/dashboard.png"
-        thumbnailAlt="Hero Video"
-        className="mt-16 max-w-screen-md rounded-lg border shadow-lg"
-      />
-    </motion.div>
   );
 }
 
